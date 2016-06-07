bb0b496ba2057377cf6f95828b5903acba82955e
Chris Karpyszyn <chris.karpyszyn@gmail.com>
2016-06-06 23:58:00 -0400

    Add database migrations - code first

--- Vidly/Models/Customer.cs
+++ Vidly/Models/Customer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,7 +9,14 @@ namespace Vidly.Models
     public class Customer
     {
         public int Id { get; set; }
+        [Required]
+        [StringLength(255)]
         public string Name { get; set; }
+        public bool IsSubscribedToNewsletter { get; set; }
+        //navigation property
+        public MembershipType MembershipType { get; set; }
+        //foreign key
+        public byte MembershipTypeId { get; set; }
 
     }
 }
