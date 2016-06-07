64dc5db65f67f273a570ce86b1dfd870dceb6645
Chris Karpyszyn <chris.karpyszyn@gmail.com>
2016-06-07 12:50:04 -0400

    Add customer form to Create and Edit new Customer

--- Vidly/Models/Customer.cs
+++ Vidly/Models/Customer.cs
@@ -9,14 +9,21 @@ namespace Vidly.Models
     public class Customer
     {
         public int Id { get; set; }
+
         [Required]
         [StringLength(255)]
         public string Name { get; set; }
+
+        [Display(Name = "Date of Birth")]
         public DateTime? Birthdate { get; set; }
+
         public bool IsSubscribedToNewsletter { get; set; }
+        
         //navigation property
         public MembershipType MembershipType { get; set; }
+
         //foreign key
+        [Display(Name = "Membership Type")]
         public byte MembershipTypeId { get; set; }
 
     }
