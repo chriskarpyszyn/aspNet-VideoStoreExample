e0a9cfb49a0ab1cabe2a9dba2fe85b426b53a5e2
Chris Karpyszyn <chris.karpyszyn@gmail.com>
2016-06-07 10:25:58 -0400

    Add Genres model. Populate table

---
+++ Vidly/Migrations/201606071418228_CreateGenreTable.cs
@@ -0,0 +1,34 @@
+namespace Vidly.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class CreateGenreTable : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Genres",
+                c => new
+                    {
+                        Id = c.Byte(nullable: false),
+                        Genre = c.String(nullable: false, maxLength: 255),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            AddColumn("dbo.Movies", "GenreId", c => c.Byte(nullable: false));
+            AddColumn("dbo.Movies", "Genres_Id", c => c.Byte());
+            CreateIndex("dbo.Movies", "Genres_Id");
+            AddForeignKey("dbo.Movies", "Genres_Id", "dbo.Genres", "Id");
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Movies", "Genres_Id", "dbo.Genres");
+            DropIndex("dbo.Movies", new[] { "Genres_Id" });
+            DropColumn("dbo.Movies", "Genres_Id");
+            DropColumn("dbo.Movies", "GenreId");
+            DropTable("dbo.Genres");
+        }
+    }
+}
